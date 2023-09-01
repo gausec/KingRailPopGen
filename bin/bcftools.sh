2f9a3b65f30fa5a04a4a444381c63e621c4a8be9
Carol Gause <112657596+gausec@users.noreply.github.com>
2023-09-01 17:08:58 -0400

    Update bcftools.sh

--- bin/bcftools.sh
+++ bin/bcftools.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Set directory
+# set working directory
 bam_directory="/home/gausec21/WholeGenomes/SortedBAM"
 
-# Loop through each *.sorted.bam file
+# loop through *.sorted.bam files
 for bam_file in "$bam_directory"/*.sorted.bam; do
     # Extract the filename without the extension
     file_prefix=$(basename "$bam_file" .sorted.bam)
 
-    # Perform the bcftools commands using the current bam_file and file_prefix
+    # bcftools commands
     bcftools mpileup -Ou -f CLRAindex/Rallus_crepitans_1.0.fasta --threads 20 "$bam_file" | bcftools call -mv -Ob -o "${file_prefix}_output_filtered.bcf"
 
 done
