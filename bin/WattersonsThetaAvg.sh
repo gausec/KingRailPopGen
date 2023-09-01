46fef4a7436d234c5c22793d493da7eb18080a99
Carol Gause <112657596+gausec@users.noreply.github.com>
2023-09-01 12:18:22 -0400

    Update WattersonsThetaAvg.sh

--- bin/WattersonsThetaAvg.sh
+++ bin/WattersonsThetaAvg.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-# Check for the input file argument
+# check for the input file argument
 if [ $# -eq 0 ]; then
   echo "Usage: $0 <input_file.thetas.idx.pestPG>"
   exit 1
 fi
 
-# Input file name
+# input file name
 input_file="$1"
 
-# Output file names
+# output file names
 tW_file="tW.txt"
 cleaned_tW_file="cleaned_tW.txt"
 average_tW_file="Average_tW.txt"
 
-# Calculate Watterson's theta (tW)
+# calculate Watterson's theta 
 awk '{print $4 / $14}' "$input_file" >> "$tW_file"
 
-# Remove empty rows
+# remove empty rows
 awk '$1 != "-nan"' "$tW_file" > "$cleaned_tW_file"
 
-# Calculate Waterson's theta by taking the average
+# calculate avg. Waterson's theta
 awk '{ sum += $1 } END { print sum / NR }' "$cleaned_tW_file" > "$average_tW_file"
 
-# Print the input file name and final value to the output file
+# print the input file name + final value to output file
 echo "Input File: $input_file" >> "$average_tW_file"
 
-# Display completion message
+# done
 echo "Process is complete. Results in Average_tW.txt"
 
